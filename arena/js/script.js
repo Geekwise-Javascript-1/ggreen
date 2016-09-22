c7d12e4b535d9ebfb51b99902504724e66f47527
silentangel7500 <silentangel77@gmail.com>
2016-09-21 22:00:23 -0700

    finished css, and started js

---
+++ arena/js/script.js
@@ -0,0 +1,61 @@
+// String = anything within quotes
+// Number
+// Float = anything with a decimal point
+// Boolean = 2 types (true or false);
+// Array
+// Object =
+/*
+var fName = "Garrett";
+var lName = new String("Green");
+var age = 27;
+var age2 = "27";
+var dist = 2.75;
+var gender = true;
+var list = ["eggs", "bread", "milk"];
+var car = {
+  make: "scion",
+  model: "FR-S",
+  year: 2013
+};
+
+
+
+console.log(fName[4]);
+console.log(lName);
+console.log(typeof age);
+console.log(typeof age2);
+console.log(typeof dist);
+console.log(typeof gender);
+console.log(list[1]);
+console.log(car.model);
+*/
+
+/*
+var fName = "Garrett";
+var lName = "Green";
+var dob = "09-10-1989";
+var hobby = "ark";
+alert("First Name:" + " " + fName + " " + "Last Name:"  + " " + " "  + lName + " " + "Birthday" + " " + " " + dob + " " + "My Hobby" + " " + hobby);
+*/
+
+/*
+var name = prompt("Enter your favorite food:");
+  console.log(name);
+  alert(name);
+*/
+
+/*
+var name = prompt("Enter your name");
+var password = prompt("Enter your password");
+
+console.log(password);
+
+var x = confirm("Is this your name?" + " " + name);
+console.log(x);
+*/
+
+/*
+var myAge = prompt("what is your age?");
+console.log(typeof parseInt(myAge));
+confirm("is this your age:"+ myAge);
+*/
