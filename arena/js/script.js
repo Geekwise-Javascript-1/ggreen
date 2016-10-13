5ddc787c4f96a23b886e65fffca99a04b9170c63
silentangel7500 <silentangel77@gmail.com>
2016-10-12 20:36:06 -0700

    started creating grid

--- arena/js/script.js
+++ arena/js/script.js
@@ -1,61 +1,90 @@
-// String = anything within quotes
-// Number
-// Float = anything with a decimal point
-// Boolean = 2 types (true or false);
-// Array
-// Object =
+var n = document.getElementById('n');
+var e = document.getElementById('e');
+var s = document.getElementById('s');
+var w = document.getElementById('w');
 /*
-var fName = "Garrett";
-var lName = new String("Green");
-var age = 27;
-var age2 = "27";
-var dist = 2.75;
-var gender = true;
-var list = ["eggs", "bread", "milk"];
-var car = {
-  make: "scion",
-  model: "FR-S",
-  year: 2013
-};
+n.addEventListener('click', function(e){
+  console.log("Go North");
+});
 
+e.addEventListener('click', function(e){
+  console.log("Go East");
+});
 
+s.addEventListener('click', function(e){
+  console.log("Go South");
+});
 
-console.log(fName[4]);
-console.log(lName);
-console.log(typeof age);
-console.log(typeof age2);
-console.log(typeof dist);
-console.log(typeof gender);
-console.log(list[1]);
-console.log(car.model);
+w.addEventListener('click', function(e){
+  console.log("Go West");
+});
 */
+n.addEventListener('click', function(e){
+  moveNorth();
+});
+function moveNorth(){
+  console.log('go north');
+}
 
-/*
-var fName = "Garrett";
-var lName = "Green";
-var dob = "09-10-1989";
-var hobby = "ark";
-alert("First Name:" + " " + fName + " " + "Last Name:"  + " " + " "  + lName + " " + "Birthday" + " " + " " + dob + " " + "My Hobby" + " " + hobby);
-*/
+e.addEventListener('click', function(e){
+  moveEast();
+});
+function moveEast(evt){
+  console.log('go east');
+}
 
-/*
-var name = prompt("Enter your favorite food:");
-  console.log(name);
-  alert(name);
-*/
+s.addEventListener('click', function(e){
+  moveSouth();
+})
+function moveSouth(){
+  console.log('move south');
+}
 
-/*
-var name = prompt("Enter your name");
-var password = prompt("Enter your password");
+w.addEventListener('click', function(e){
+  moveWest();
+});
+function moveWest(){
+  console.log('move west');
+}
 
-console.log(password);
 
-var x = confirm("Is this your name?" + " " + name);
-console.log(x);
-*/
+var grid = function(x, y){
+  var totalCells = x * y;
+  var cells = [];
+  var unvisited = [];
+  // console.log(x + ":" + y);
+  for(var i = 0; i < y; i++){
+      cells[i] = [];
+      unvisited[i] = [];
+      for(var j = 0; j < x; j++){
+        cells[i][j] = [0, 0, 0, 0];
+        unvisited[i][j] = true;
+      }
+  }
+  var currentCell = [Math.floor(Math.random() * y), Math.floor(Math.random() * x)];
+  var path = [currentCell];
+  unvisited[currentCell[0]][currentCell[1]] = false;
 
-/*
-var myAge = prompt("what is your age?");
-console.log(typeof parseInt(myAge));
-confirm("is this your age:"+ myAge);
-*/
+  var noOfVisited = 1;
+
+  while(noOfVisited < totalCells){
+    var possible = [[currentCell[0]-1, currentCell[1], 0, 2], // we are checking if the y value of neighbor cell (top) is inside of the grid
+                    [currentCell[0], currentCell[1]+1, 1, 3], // check if x value of neighbor of cell (right) is inside the grid
+                    [currentCell[0]+1, currentCell[1], 2, 0], //checking y value of neighbor (below) current cell is in the grid
+                    [currentCell[0]-1], [currentCell[1]-1, 3, 1]]; //checking for the cell (left) of the current cell inside the grid
+    var neighbors = [];
+
+    for(var l = 0; l < 4; l++){
+      if(possible[l][0] > -1 &&
+        possible[l][0] < y &&
+        possible[l][1] > -1 &&
+        possible[l][1] < x &&
+        unvisited[possible[l][0]][possible[l][1]]){
+          neighbors.push(possible[l]);
+        }
+    }
+  }
+
+
+}(4, 4);
+//function(){} /***  ()   ****/; automatically runs the function witout needing to call it seperately
